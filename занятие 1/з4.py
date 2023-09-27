7102fa456a8b02371821473c7f4f98fcd383b936
siyavdi <ziyavdimovsarov11@yandex.ru>
2023-09-27 23:20:33 +0300

    занятие 1
    занятие 1

---
+++ занятие 1/з4.py
@@ -0,0 +1,7 @@
+seconds = int(input('Введите количество секунд'))
+Введите количество секунд54324321
+minutes = seconds//60
+hours = minutes//60
+days = hours//24
+print('days,hours,minutes,seconds')
+days,hours,minutes,seconds
