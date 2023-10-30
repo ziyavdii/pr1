9281f7e89278a2f768587015d7209a13a59b7585
siyavdi <ziyavdimovsarov11@yandex.ru>
2023-10-30 21:46:25 +0300

    Revert "занятие 5"
    This reverts commit 95f77bd85e16f41804da5fc78ef0a0b86d3f9447.

--- занятие 5/2 задание.py
+++
@@ -1,8 +0,0 @@
-def min(n):
-    i = 2
-    while n % i != 0:
-        i += 1
-    return i
-n = int(input())
-r = min(n)
-print(r)
--- занятие 5/5 задание.py
+++
@@ -1,8 +0,0 @@
-def code(c):
-    n = 0
-    while int(input()) != 0:
-        n += 1
-    return n
-c=int(input())
-r = code(c)
-print(r)
